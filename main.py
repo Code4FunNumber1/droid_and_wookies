7905ef9c3b0fedd0a33059d7de6d21be04303ae1
Jacob A. Stephens <stephjac000@warren.k12.in.us>
2024-08-29 09:44:08 -0400

    inntial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+droids = input("How many droids do you want to meet: ")
+wookies= input("How many wookies do you want to meet: ")
+print(name, "wants to meet", droids, "droids and ", "Wookies!")
